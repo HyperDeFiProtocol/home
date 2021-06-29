532686977d8fc81211d6dbd53484b3e6d16ba8c0
John Torres <apodpm@gmail.com>
2021-06-29 21:48:24 +0800

    add HeroIconOutlineSpeakerphone and use it for Telegram Channel

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -32,14 +32,14 @@
 <!--          <IconInstagram class="h-6 w-6" />-->
 <!--        </a>-->
 
+        <a target='_blank' :href='hdfLink.telegram(telegramChannel)' class="text-gray-400 hover:text-gray-500">
+          <span class="sr-only">Telegram Channel</span>
+          <HeroIconOutlineSpeakerphone class="h-6 w-6" />
+        </a>
+
         <a target='_blank' :href='hdfLink.telegram()' class="text-gray-400 hover:text-gray-500">
           <span class="sr-only">Telegram</span>
           <IconTelegram class="h-6 w-6" />
-        </a>
-
-        <a target='_blank' :href='hdfLink.telegram(telegramChannel)' class="text-gray-400 hover:text-gray-500">
-          <span class="sr-only">Telegram Channel</span>
-          <IconTelegramCircle class="h-6 w-6" />
         </a>
 
         <a target='_blank' :href='hdfLink.twitter()' class="text-gray-400 hover:text-gray-500">
