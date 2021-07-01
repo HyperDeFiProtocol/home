5127c31d658c39d7e01887ffd05ed297aab0226c
John Torres <apodpm@gmail.com>
2021-07-01 20:41:32 +0800

    fix: Plugin[init-web3]

--- plugins/init-web3.js
+++ plugins/init-web3.js
@@ -6,14 +6,29 @@ export default async function ({ app, store }, inject) {
   // app.myInjectedFunction = string =>
   //   console.log('Okay, another function', string)
 
+  await store.dispatch('bsc/SET_WEB3', new Web3(process.env.web3RpcUrl))
+  await store.dispatch('bsc/SET_CHAIN_ID')
+  let goodChain = true
+
   const provider = await detectEthereumProvider()
   if (provider) {
-    await store.dispatch('bsc/SET_WEB3', new Web3(window.ethereum))
-  } else {
-    await store.dispatch('bsc/SET_WEB3', new Web3(process.env.web3RpcUrl))
+    const web3 = new Web3(window.ethereum)
+
+    let chainId = await web3.eth.getChainId().catch(error => {
+      goodChain = false
+      console.error('>>> Plugin[init-web3] getChainId:', error.message)
+    })
+
+    if (store.state.bsc.chainId !== chainId) {
+      goodChain = false
+    }
+
+    if (goodChain) {
+      await store.dispatch('bsc/SET_WEB3', web3)
+      await store.dispatch('wallet/SET_CHAIN_ID')
+    }
   }
 
-  await store.dispatch('bsc/SET_CHAIN_ID')
   await store.dispatch('bsc/SYNC_BLOCK_NUMBER')
   await store.dispatch('bsc/SYNC_GAS_PRICE')
   await store.dispatch('bsc/SYNC_DATA')
