5bad494ebae7340b5bc15a8a9f8574a43063bbc9
John Torres <apodpm@gmail.com>
2021-07-03 21:51:52 +0800

    update: bg-color/opacity for modals

--- components/ModalLang.vue
+++ components/ModalLang.vue
@@ -2,7 +2,7 @@
   <!-- This example requires Tailwind CSS v2.0+ -->
   <div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true" @click='close'>
     <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-      <div class="transition duration-300 ease-in-out fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+      <div class="transition duration-300 ease-in-out fixed inset-0 bg-black bg-opacity-80 transition-opacity" aria-hidden="true"></div>
 
       <!-- This element is to trick the browser into centering the modal contents. -->
       <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
