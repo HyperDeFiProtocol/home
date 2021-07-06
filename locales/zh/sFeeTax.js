a80251f978cbadcf3de06ff00391235505755427
John Torres <apodpm@gmail.com>
2021-07-06 22:46:12 +0800

    update: zh for sFeeTax

--- locales/zh/sFeeTax.js
+++ locales/zh/sFeeTax.js
@@ -4,7 +4,7 @@ export default {
 
   takeThen__: '然后存入税收矿池；每个 HyperDeFi 持有者都能按比例分享，这意味着您可以通过简单的持有，来赚取更多的 HyperDeFi，每周收矿，甚至每天。',
 
-  track__: '跟踪所有税收和挖矿转账',
+  track__: '跟踪所有税收和收矿转账',
 
   totalTax: '挖矿产出',
   totalTaxMarketValue: '挖矿总市值',
