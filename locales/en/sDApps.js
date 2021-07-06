2b2710c1f79526888229ffb4c9de6c942b7b8264
John Torres <apodpm@gmail.com>
2021-07-07 00:57:08 +0800

    update: SUsername, SDApps

--- locales/en/sDApps.js
+++ locales/en/sDApps.js
@@ -1,5 +1,5 @@
 export default {
   tag: 'DApps',
   title: 'About to launch: 6 official DApps',
-  desc: 'Under Development...',
+  desc: 'Upcoming...',
 }
--- locales/en/sUsername.js
+++ locales/en/sUsername.js
@@ -1,6 +1,6 @@
 export default {
   title: 'Enable your personal dashboard',
-  desc: 'Set an username to enable your personal dashboard; it\'s easier to remember than a wallet address, and can also been used for upcoming DApps.',
+  desc: 'Set an username to enable your personal dashboard; an username is much easier to remember than a wallet address, and will also been used for upcoming DApps.',
 
   sr: 'Set your username',
   placeholder: 'Enter your username',
