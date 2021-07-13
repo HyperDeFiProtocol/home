1a0b4e73502fbda43a6e84f2421d63326a6db8cb
John Torres <apodpm@gmail.com>
2021-07-13 15:01:37 +0800

    add: IconBUSD

---
+++ components/IconBUSD.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+    <g fill='#f0b90b'>
+      <path d="M12,1.97l2.478,2.537l-6.239,6.239l-2.478,-2.477l6.239,-6.299Z"/>
+      <path d="M15.761,5.731l2.478,2.538l-10,10l-2.478,-2.478l10,-10.06Z"/>
+      <path d="M4.478,9.493l2.477,2.537l-2.477,2.477l-2.478,-2.477l2.478,-2.537Z"/>
+      <path d="M19.522,9.493l2.478,2.537l-10,10l-2.478,-2.478l10,-10.059Z"/>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IconBUSD'
+}
+</script>
+
+<style scoped>
+
+</style>
