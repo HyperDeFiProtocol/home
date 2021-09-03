8c56df77d3693746f74433b1553b61bc0536e915
John Torres <apodpm@gmail.com>
2021-09-03 23:46:32 +0800

    update: token avatar

--- plugins/web3.js
+++ plugins/web3.js
@@ -69,7 +69,7 @@ export default async function({ app, store }, inject) {
             address: process.env.tokenAddress,
             symbol: store.state.bsc.metadata.tokenSymbol,
             decimals: parseInt(store.state.bsc.metadata.tokenDecimals),
-            image: 'https://hyperdefi.org/android-chrome-192x192.png'
+            image: 'https://hyperdefi.org/avatar.png'
           }
         }
       })
