56647bc8c2738f74f23eb481c3f3505f14b729c8
John Torres <apodpm@gmail.com>
2021-09-03 12:52:17 +0800

    #62, update: /address/_address

--- pages/address/_address.vue
+++ pages/address/_address.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <LAutoWidth v-if='account.username' class='py-16 px-4 sm:py-24 sm:px-6 lg:px-8'>
+    <LAutoWidth v-if='account.isHolder' class='py-16 px-4 sm:py-24 sm:px-6 lg:px-8'>
       <CH3>
-        Hello, "{{ indexedUsername }}"
+        User Dashboard
 
         <template #tag>
           User Dashboard
@@ -10,7 +10,7 @@
 
         <template #desc>
           <div class='truncate'>
-            {{ account.address }}
+            {{ address }}
           </div>
         </template>
       </CH3>
@@ -23,28 +23,23 @@
         <HeroIconOutlineExclamation class='h-20 w-20' />
       </div>
 
-      <div v-if='$route.params.username.startsWith("@")'>
+      <div v-if='checksumAddress'>
         <h1 class='mt-10 font-mono font-bold text-4xl'>
-          Username does not exist
+          Not a holder
         </h1>
 
-        <h2 class='mt-6 text-2xl'>
-          "{{ username }}" could not be found
+        <h2 class='mt-6 text-2xl break-all'>
+          "{{ $route.params.address }}" is not a HyperDeFi holder
         </h2>
       </div>
 
       <div v-else>
         <h1 class='mt-10 font-mono font-bold text-4xl'>
-          Invalid username
+          Invalid address
         </h1>
 
-        <h2 class='mt-6 text-2xl'>
-          <span>
-            A username must start with an "@"
-          </span>
-          <span v-if='$route.params.username'>
-            but got "{{ $route.params.username }}"
-          </span>
+        <h2 class='mt-6 text-2xl break-all'>
+          "{{ $route.params.address }}" is not a valid address
         </h2>
       </div>
     </LAutoWidth>
@@ -52,14 +47,15 @@
 </template>
 
 <script>
+import Web3 from 'web3'
 import fn from '~/utils/functions'
 
 export default {
   scrollToTop: true,
-  name: 'UserUsername',
+  name: 'UserAddress',
   async asyncData({ params }) {
-    const username = params.username
-    return { username }
+    const address = params.address
+    return { address }
   },
   data() {
     return {
@@ -84,13 +80,16 @@ export default {
     }
   },
   computed: {
-    indexedUsername() {
-      if (this.username.startsWith('@')) {
-        return this.username.slice(1)
+    isAddress() {
+      return Web3.utils.isAddress(this.address)
+    },
+    checksumAddress() {
+      if (this.isAddress) {
+        return Web3.utils.toChecksumAddress(this.address)
       }
 
-      return ''
-    }
+      return null
+    },
   },
   mounted: async function () {
     await fn.wait(500)
@@ -98,13 +97,17 @@ export default {
   },
   methods: {
     async sync() {
-      const data = await this.$nuxt.context.app.token.methods.getAccountByUsername(this.indexedUsername).call()
+      if (!this.isAddress) {
+        return
+      }
+
+      const data = await this.$nuxt.context.app.token.methods.getAccount(this.checksumAddress).call()
         .catch(error => {
           console.error('>>> P[/user/_username] getAccountByUsername:', error.message)
         })
 
       if (data.account !== this.$store.state.bsc.globalAccounts.zero) {
-        this.account.address = data.account
+        this.account.address = this.checksumAddress
 
         this.account.isHolder = data.isHolder
         this.account.isWhale = data.isWhale
