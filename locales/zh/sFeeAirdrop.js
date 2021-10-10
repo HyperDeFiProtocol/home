0469fd0c681a46f8cefbfedbede3c57f394992ce
John Torres <apodpm@gmail.com>
2021-10-10 11:12:53 +0800

    update: zh: sFeeAirdrop

--- locales/zh/sFeeAirdrop.js
+++ locales/zh/sFeeAirdrop.js
@@ -1,6 +1,6 @@
 export default {
   title: '随机空投',
-  text: '持有即空投，每一位 HyperDeFi 的持有者都有机会获得，可以多次获得，没有上限。',
+  text: '持有即空投，每一位 HyperDeFi 的持有者都有机会，可以多次获得，没有上限。',
 
   takeThen__: '然后随机发送给 1 位持有人（最低持有量',
   takeThen2__: '）。',
