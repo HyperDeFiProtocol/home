959365146105f94b5ac4a513bd4eefd1b57f49a5
John Torres <apodpm@gmail.com>
2021-10-18 19:23:09 +0800

    update: sWelcome

--- locales/en/sWelcome.js
+++ locales/en/sWelcome.js
@@ -5,7 +5,7 @@ export default {
   hyperDeFiProtocol: 'HyperDeFi Protocol',
   text: 'Holding is farming! The Open-sourced HyperDeFi Protocol is community driven, fair launched. Some functions occur during each trade: Airdrop, FOMO, LP Acquisition, Fund, Burn, and global Tax share to every holders; and ready for more DApps...',
   buyOnPancakeSwap: 'Buy on PancakeSwap',
-  visitGenesis: 'Visit Pre-sale',
+  visitGenesis: 'Visit Genesis',
   _providedBy: 'The latest price is provided by',
   onBlockNumber: 'on block number',
   of: 'of',
