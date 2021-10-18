e1a133a94c12eef88e301c946e8031839a51c0be
John Torres <apodpm@gmail.com>
2021-10-18 19:33:24 +0800

    update: sWelcome

--- locales/en/sWelcome.js
+++ locales/en/sWelcome.js
@@ -3,7 +3,7 @@ export default {
   tradeHyperDeFi: 'Trade HyperDeFi',
   welcomeTo: 'Welcome to',
   hyperDeFiProtocol: 'HyperDeFi Protocol',
-  text: 'Holding is farming! The Open-sourced HyperDeFi Protocol is community driven, fair launched. Some functions occur during each trade: Airdrop, FOMO, LP Acquisition, Fund, Burn, and global Tax share to every holders; and ready for more DApps...',
+  text: 'Holding is farming! The Open-sourced HyperDeFi Protocol is community driven, fair launched. Some functions occur during each trade: Randomly Airdrop, FOMO, LP Acquisition, Fund, Burn, then share to every holders; and ready for more DApps...',
   buyOnPancakeSwap: 'Buy on PancakeSwap',
   visitGenesis: 'Visit Genesis',
   _providedBy: 'The latest price is provided by',
--- locales/zh/sWelcome.js
+++ locales/zh/sWelcome.js
@@ -3,7 +3,7 @@ export default {
   tradeHyperDeFi: '交易 HyperDeFi',
   welcomeTo: '欢迎来到',
   hyperDeFiProtocol: 'HyperDeFi 协议',
-  text: '持有即挖矿！开源的 HyperDeFi 协议由社区驱动，公平启动。每笔交易中收取费用：空投、FOMO、流动池回收、基金、销毁、和全球税分享给每个持有人；并已为 DApp 做好准备……',
+  text: '持有即挖矿！开源的 HyperDeFi 协议由社区驱动，公平启动。每笔交易中收取交易税，用于矿池、随机空投、FOMO、流动池回收、基金、销毁，分享给每个持有人；并已为 DApp 做好准备……',
   buyOnPancakeSwap: '在 PancakeSwap 购买',
   visitGenesis: '进入创世纪',
   _providedBy: '最新价格由',
