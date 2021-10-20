0e336178df38f813119dd682b2e248abc1e90722
John Torres <apodpm@gmail.com>
2021-10-21 03:27:16 +0800

    #106 update: sFAQ

--- locales/en/sFAQ.js
+++ locales/en/sFAQ.js
@@ -2,22 +2,22 @@ export default {
   title: 'Frequently Asked Questions',
   desc: 'Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec. Urna, sed a lectus elementum blandit et.',
 
-  qFee: 'How does the transaction tax work?',
-  aFee1: 'HyperDeFi has a 10%-30% transaction tax (',
-  aFee2: '); and all of it will be returned to the holders and the ecology in different ways. Please check the Tax - Universal Dividend Section on the home page.',
+  qFee: 'How does the ecological tax work?',
+  aFee1: 'HyperDeFi has a 10%-30% ecological tax (',
+  aFee2: '); and all the tax will be returned to the holders and the ecology in different ways. Please check the Ecological Tax - Universal Dividend Section on the home page.',
 
   fromTaker: 'from taker',
   fromMaker: 'from maker',
   fromWhale: 'from whale',
 
   qBuyAndSafety: 'How do I buy HyperDeFi? Is it safe?',
-  aBuyAndSafety1: 'HyperDeFi is based on BSC Chain (Binance Smart Chain) and the liquidity is locked in PancakeSwap. It is completely safe. You can get HyperDeFi from',
+  aBuyAndSafety1: 'HyperDeFi based on BSC Chain (Binance Smart Chain) and the liquidity is locked in PancakeSwap. It is completely safe. You can swap HyperDeFi from',
   aBuyAndSafety2: '.',
 
   qDifferent: 'What makes HyperDeFi different from other tokens?',
   aDifferent: 'Holding is farming; 100% decentralized; no reservation for team nor anyone; more healthier 3 trade fee levels for Taker, Maker, and Whale, heavy tax for scripts; a FOMO prize is accumulating there to incentive trading consistently; and even a auto-burning scheme to keep making HyperDeFi deflationary in the long term; and one more thing: the upcoming DApps.',
 
   qAutoFarming: 'Is the HyperDeFi auto-farming?',
-  aAutoFarming: 'Yes, Safe Auto-farming: almost all the transaction fees will be returned to holders and the ecology in different ways. You will see your harvest (a wealth accumulation) in the wallet right away once you start holding.',
+  aAutoFarming: 'Yes, Safe Auto-farming: almost all the ecological tax will be returned to holders and the ecology in different ways. You will see your harvest (a wealth accumulation) in the wallet right away once you start holding.',
 
 }
