ae83469aaaa845e3ab2adf947689a90ebb32ed7a
John Torres <apodpm@gmail.com>
2021-10-21 03:19:39 +0800

    #106 update: SUsername

--- locales/en/sUsername.js
+++ locales/en/sUsername.js
@@ -1,6 +1,6 @@
 export default {
   title: 'Enable your personal dashboard',
-  desc: 'Set an username to enable your personal dashboard; an username is much easier to remember than a wallet address, and will also been used for upcoming DApps.',
+  desc: 'Set an username to enable your personal dashboard to check your harvest, airdrop, FOMO and coupon bonus history; an username is much easier to remember than a wallet address, and it will also be used for upcoming DApps',
 
   sr: 'Set your username',
   placeholder: 'Your username',
