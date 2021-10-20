7b0f7a3fb2fcfb1712cce902f57406b0e6a07a53
John Torres <apodpm@gmail.com>
2021-10-21 02:57:53 +0800

    #106 update: SReadyForDApps

--- locales/en/sReadyForDApps.js
+++ locales/en/sReadyForDApps.js
@@ -1,7 +1,7 @@
 export default {
   tag: 'Slot',
   title: 'Ready for more DApps',
-  desc: 'Interface is provided, for building DApps with HyperDeFi functions',
+  desc: 'Interface is provided for developers to build DApps with HyperDeFi ecological tax functions',
 
   documentation: 'Documentation',
   githubCodes: 'GitHub Codes',
--- locales/zh/sReadyForDApps.js
+++ locales/zh/sReadyForDApps.js
@@ -1,7 +1,7 @@
 export default {
   tag: '插槽',
   title: '已为更多 DApp 准备就续',
-  desc: '预留接口和插槽，可构建带有 HyperDeFi 功能的 DApp',
+  desc: '为开发人员预留接口和插槽，可构建带有 HyperDeFi 生态税功能的 DApp',
 
   documentation: '文档',
   githubCodes: 'GitHub 代码',
