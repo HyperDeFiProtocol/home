df8510df3bd35027b825caf0ed45f0d374e401f3
John Torres <apodpm@gmail.com>
2021-10-22 00:50:16 +0800

    update: sTaxAirdrop

--- locales/zh/sTaxAirdrop.js
+++ locales/zh/sTaxAirdrop.js
@@ -2,7 +2,7 @@ export default {
   title: '随机空投',
   text: '持有即空投，每一位 HyperDeFi 的持有者都有机会多次获得，没有上限。',
 
-  takeThen__: '然后随机发送给 1 位持有人（最低持有量',
+  takeThen__: '然后随机发送给 1 位持有人/幸运地址（最低持有量',
   takeThen2__: '）。',
   once_there__: '每一笔交易完成，都有一位持有人可以获得空投。',
 
